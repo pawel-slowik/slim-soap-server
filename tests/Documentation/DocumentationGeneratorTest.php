fa509d9cffe94e686adfca2a9696cefba857f59d
Paweł Słowik <pawel.slowik@zoho.com>
2019-10-10 15:40:03 +0200

    Add test for DocumentationGenerator

---
+++ tests/Documentation/DocumentationGeneratorTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Documentation;
+
+use PHPUnit\Framework\TestCase;
+
+use Application\Models\SoapService;
+use Application\Models\DocumentationGenerator;
+use Application\Models\DocumentedService;
+
+/**
+ * @covers \Application\Models\DocumentationGenerator
+ */
+class DocumentationGeneratorTest extends TestCase
+{
+    protected $soapService;
+
+    protected function setUp(): void
+    {
+        $this->soapService = new SoapService(new Hello());
+    }
+
+    public function testReturnedType(): void
+    {
+        $generator = new DocumentationGenerator();
+        $documentedService = $generator->createDocumentation($this->soapService);
+        $this->assertInstanceOf(DocumentedService::class, $documentedService);
+    }
+}
