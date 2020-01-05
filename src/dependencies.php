513da973edb857503839ab8e7a8de711d86cd583
Paweł Słowik <pawel.slowik@zoho.com>
2020-01-05 23:17:17 +0100

    Use an array for setting up dependencies

--- src/dependencies.php
+++ src/dependencies.php
@@ -13,31 +13,34 @@ use ExampleServices\Hello;
 use ExampleServices\ExampleServiceWithComplexTypes;
 
 return function (App $app): void {
-    // TODO: more DI improvements?
+    $dependencies = [
 
-    $container = $app->getContainer();
+        // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
+        RouteParserInterface::class => function ($container) use ($app) {
+            return $app->getRouteCollector()->getRouteParser();
+        },
 
-    // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
-    $container->set(RouteParserInterface::class, function ($container) use ($app) {
-        return $app->getRouteCollector()->getRouteParser();
-    });
+        // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
+        Twig::class => function ($container) {
+            return Twig::create(
+                "templates",
+                [
+                    "cache" => "/tmp/twig_cache",
+                    "auto_reload" => true,
+                ]
+            );
+        },
 
-    // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
-    $container->set(Twig::class, function ($container) {
-        return Twig::create(
-            "templates",
-            [
-                "cache" => "/tmp/twig_cache",
-                "auto_reload" => true,
-            ]
-        );
-    });
+        // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
+        SoapServiceRegistry::class => function ($container) {
+            $reg = new SoapServiceRegistry();
+            $reg->addService("hello", new SoapService(new Hello()));
+            $reg->addService("complex", new SoapService(new ExampleServiceWithComplexTypes()));
+            return $reg;
+        },
+    ];
 
-    // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
-    $container->set(SoapServiceRegistry::class, function ($container) {
-        $reg = new SoapServiceRegistry();
-        $reg->addService("hello", new SoapService(new Hello()));
-        $reg->addService("complex", new SoapService(new ExampleServiceWithComplexTypes()));
-        return $reg;
-    });
+    foreach ($dependencies as $dependency => $factory) {
+        $app->getContainer()->set($dependency, $factory);
+    }
 };
